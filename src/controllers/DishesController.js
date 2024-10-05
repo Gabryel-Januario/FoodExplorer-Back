406f2877b09a80610ad2d72cfa3ed3ad8c11047c
Gabryel Januario <gabryeelcomy@gmail.com>
2024-10-05 15:21:16 -0300

    display/delete dishes

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -1,3 +1,4 @@
+const { response } = require("express")
 const knex = require("../database/knex")
 
 class DishesController {
@@ -5,7 +6,7 @@ class DishesController {
     const { title, description, price, ingredients } = req.body
     const { user_id } = req.params
 
-    const { dishe_id } = await knex("dishes").insert({
+    const [dishe_id] = await knex("dishes").insert({
       title,
       description,
       price,
@@ -23,6 +24,28 @@ class DishesController {
     await knex("ingredients").insert(ingredientsInsert)
 
     res.json()
+  }
+
+  async show(req, res) {
+    const { id } = req.params
+
+    const dishe = await knex("dishes").where({ id }).first()
+    const ingredients = await knex("ingredients")
+      .where({ dishe_id: id })
+      .orderBy("name")
+
+    return res.json({
+      ...dishe,
+      ingredients,
+    })
+  }
+
+  async delete(req, res) {
+    const { id } = req.params
+
+    await knex("dishes").where({ id }).delete()
+
+    return res.json()
   }
 }
 
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -6,5 +6,7 @@ const dishesRoutes = Router()
 const dishesController = new DishesController()
 
 dishesRoutes.post("/:user_id", dishesController.create)
+dishesRoutes.get("/:id", dishesController.show)
+dishesRoutes.delete("/:id", dishesController.delete)
 
 module.exports = dishesRoutes
